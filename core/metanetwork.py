9e4d2c6cee8346bfaec130b2523c67d244afed07
Volodymyr Rudyj <vladimir.rudoy@gmail.com>
2011-07-13 11:06:45 +0300

    Added core classes prototypes

---
+++ core/metanetwork.py
@@ -0,0 +1,49 @@
+class MetaSocial(object):
+    def __init__(self, service_type, service_params):
+        pass
+
+    def getAuthorizedPerson(self):
+        pass
+
+    def getPerson(self, person_id):
+        pass
+
+    def getServiceName(self):
+        pass
+
+    def getServiceDescription(self):
+        pass
+
+    def getServiceUrl(self):
+        pass
+
+
+class PersonId(object):
+    pass
+
+class Person(object):
+
+    def getId(self):
+        pass
+
+    def getDisplayName(self):
+        pass
+
+    def getFirstName(self):
+        pass
+
+    def getLastName(self):
+        pass
+
+    def getGender(self):
+        pass
+
+    def getBirthDate(self):
+        pass
+
+    def getEmail(self):
+        pass
+
+    def getContactInfo(self):
+        pass
+
