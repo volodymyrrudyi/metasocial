8d7c962e7ec84a9063808556863585a86fbc18f6
Volodymyr Rudyj <vladimir.rudoy@gmail.com>
2011-07-17 21:25:16 +0300

    added twitter adapter

--- core/adapters/twitter_adapter.py
+++ core/adapters/twitter_adapter.py
@@ -4,17 +4,7 @@ from factory import registerAdapter
 from twitter import Api as TwitterAPI
 
 class TwitterAdapter(AbstractAdapter):
-    """
-    Twitter adapter class. Inherits AbstractAdapter
-    """
-
-    """
-    Access token that is used to retrieve information from twitter
-    """
     twitter_api    = None
-    """
-    Twitter Api object
-    """
 
     def __init__(self, params):
         self.access_token_key       = params["access_token_key"]
@@ -29,36 +19,20 @@ class TwitterAdapter(AbstractAdapter):
         return TwitterPerson(user_object)
 
     def getPerson(self, person_id):
-        """
-        Get instance of Person class which represents network user with specified id
-            :param person_id: Identifier of the user
-        """
         user_object = self.twitter_api.GetUser(person_id)
         return TwitterPerson(user_object)
 
     def getServiceName(self):
-        """
-        Get name of the social network
-        """
         return "Twitter Social Network"
 
     def getServiceDescription(self):
-        """
-        Get description of the social network
-        """
         pass
 
     def getServiceUrl(self):
-        """
-        Get URL to social network website
-        """
         pass
 
 
 class TwitterPerson(AbstractPerson):
-    """
-    Implementation of Person class for twitter adapter
-    """
     def __init__(self, user_object):
         print user_object.__str__()
 
